6804fc62d7b02714c59771cc6b71143be3d7748f
nezhuka <ingasom63@gmail.com>
2023-06-11 16:18:25 +0100

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let cityElement = document.querySelector("#city");
@@ -10,7 +33,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML= Math.round(response.data.wind.speed);
-    
+   let dateElement = document.querySelector("#date");
+   dateElement.innerHTML = formatDate(response.data.dt * 1000); 
 }
 
 let apiKey="d4aba3dd194534017243141841b8b193";
