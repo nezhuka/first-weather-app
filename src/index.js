6dfab5c4ea41a82e7ad7369d5213b1b0316128c5
nezhuka <ingasom63@gmail.com>
2023-06-13 13:22:11 +0100

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -40,10 +40,28 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description); 
 }
-
-let apiKey="d4aba3dd194534017243141841b8b193";
+let apiKey = "d4aba3dd194534017243141841b8b193";
 let city = "Melitopol";
-let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
 axios.get(apiUrl).then(displayTemperature);
+
+function search(city){
+ let apiKey = "d4aba3dd194534017243141841b8b193";
+ 
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+ 
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+
+}
+search("Melitopol")
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
