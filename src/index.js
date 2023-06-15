c4565be3ca0457548123a3022618010f93982b62
nezhuka <ingasom63@gmail.com>
2023-06-15 22:57:28 +0100

    Added weather forecast template to JS

--- src/index.js
+++ src/index.js
@@ -20,7 +20,36 @@ function formatDate(timestamp){
     let day = days[date.getDay()];
     return `${day}, ${hours}:${minutes}`
 }
+function displayWeatherForecast(){
+    let forecastElement = document.querySelector("#forecast");
 
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = `<div class="row">`;
+   days.forEach(function(day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+
+        <img
+          src="https://openweathermap.org/img/wn/10n@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">18°</span>
+          <span class="weather-forecast-temperature-min">12° </span>
+        </div>
+      </div>
+    `;
+   } )
+   
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+console.log(forecastHTML);
+}
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let temperatureElement=document.querySelector("#temperature");
@@ -89,3 +118,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("Melitopol");
+displayWeatherForecast();
